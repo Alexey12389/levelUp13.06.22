c0f48e6044ea98a4a4f1b29226aee4fa6397e52c
Alexey Kazakov <89110890324monty@gmail.com>
2022-12-04 19:19:43 +0300

    create_autorization

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+    <div class="app-container bg-light">
+        <nav v-show="authStore.user" class="navbar navbar-light" style="background-color: #e3f2fd;">
+            <div class="navbar-nav">
+                <RouterLink to="/" class="nav-item nav-link">Home</RouterLink>
+                <a @click="authStore.logout()" class="nav-item nav-link">Logout</a>
+            </div>
+        </nav>
+        <div class="container pt-4 pb-4">
+            <RouterView />
+        </div>
+    </div>
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
---
+++ src/stores/auth.store.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+
+import { fetchWrapper, router } from '@/helpers';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
+
+export const useAuthStore = defineStore({
+    id: 'auth',
+    state: () => ({
+        user: JSON.parse(localStorage.getItem('user')),
+        returnUrl: null
+    }),
+    actions: {
+        async login(username, password) {
+            const user = await fetchWrapper.post(`${baseUrl}/authenticate`, { username, password });
+            this.user = user;
+            localStorage.setItem('user', JSON.stringify(user));
+
+            router.push(this.returnUrl || '/');
+        },
+        logout() {
+            this.user = null;
+            localStorage.removeItem('user');
+            router.push('/login');
+        }
+    }
+});
